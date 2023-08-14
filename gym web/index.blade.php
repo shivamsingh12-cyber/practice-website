cc492fe606b9551d263fc73e85625ac5ef6cce42
Shivam_Singh <shivamsingh6490@gmail.com>
2023-08-14 23:14:34 +0530

    Developed a new gym website for practice using html,css only

---
+++ gym web/index.blade.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shivam Fitness</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/utils.css">
+</head>
+
+<body>
+    <div class="container mx-auto">
+        <header>
+            <nav class="flex justify-between">
+                <div class="logo font-bold flex items-center text-blue">Shivam Fitness</div>
+                <ul class="navbar flex items-center">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact Us</li>
+                    <li><button class="btn">Join Now</button></li>
+                </ul>
+            </nav>
+            <hr>
+        </header>
+        <main class="min-h-screen ">
+            <section class="section1">
+                <div class="flex items-center">
+                    <div class="topleft flex justify-center flex-col px-2">
+                        <img src="img/dumbell.jpg" alt="logo1" class="dumbellimg">
+                        <h1 class="my-1 txt-center">The best gym in the town is here</h1>
+                        <p class="txt-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit iste beatae
+                            libero tenetur unde distinctio. Quidem dicta in et doloribus officia, maiores velit
+                            similique eos fugit rerum commodi? Nostrum suscipit repellendus, eaque commodi possimus
+                            accusamus placeat dolorem deleniti magni quos ea natus aspernatur iusto, quaerat nobis
+                            libero pariatur culpa? Cum soluta</p>
+                    </div>
+                    <div class="topright">
+                        <img src="img/gym-1.jpg" alt="image" class="mainLogo">
+                    </div>
+                </div>
+            </section>
+            <hr>
+            <section class="section2">
+                <h1 class="txt-center my-2">Pricing</h1>
+                <p class="paracenter my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quis numquam
+                    ipsum optio tenetur praesentium vero dolorum minima quo ex fugit eius ab dolorem earum assumenda
+                    dignissimos veniam molestias qui? Quis dolorem repellat non reiciendis vel ipsam soluta. Dicta nisi
+                    rerum dolorum quis?</p>
+                <div class="boxes flex">
+                    <div class="box">
+                        <h2>Free</h2>
+                        <ul>
+                            <li>100</li>
+                            <li class="highlighted">₹0/month</li>
+                            <li>0 users included</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                            <li><button class="btn">Signup Now</button></li>
+                        </ul>
+                    </div>
+                    <div class="box">
+                        <h2>Pro</h2>
+                        <ul>
+                            <li>150</li>
+                            <li class="highlighted">₹0/month</li>
+                            <li>0 users included</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                            <li><button class="btn">Signup Now</button></li>
+                        </ul>
+                    </div>
+                    <div class="box">
+                        <h2>Enterprise</h2>
+                        <ul>
+                            <li>500</li>
+                            <li class="highlighted">₹0/month</li>
+                            <li>0 users included</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                            <li><button class="btn">Signup Now</button></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <hr>
+            <section class="section3">
+                <h1 class="txt-center my-2">Compare Plans</h1>
+                <div class="container pantTable">
+                    <table class="table txt-center">
+                        <thead>
+                          <tr>
+                            <th></th><th>Free</th>
+                            <th>Pro</th>
+                            <th>Enterprise</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row" class="text-start">Public</th>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                          </tr>
+                          <tr>
+                            <th scope="row" class="text-start">Private</th>
+                            <td>-</td>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                          </tr>
+                        </tbody>
+                
+                        <tbody>
+                          <tr>
+                            <th scope="row" class="text-start">Permissions</th>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                          </tr>
+                          <tr>
+                            <th scope="row" class="text-start">Sharing</th>
+                            <td>-</td>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                          </tr>
+                          <tr>
+                            <th scope="row" class="text-start">Unlimited members</th>
+                            <td>-</td>
+                            <td>Yes</svg></td>
+                            <td>Yes</svg></td>
+                          </tr>
+                          <tr>
+                            <th scope="row" class="text-start">Extra security</th>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>Yes</svg></td>
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
+            </section>
+            <hr>
+        </main>
+        <footer>  Copyright &copy; ShivamFitness.com | All rights reserved</footer>
+    </div>
+</body>
+
+</html>
